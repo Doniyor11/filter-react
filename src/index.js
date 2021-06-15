b90f637bb14f82a8dbe50f1fe8561cf6dc60131c
doniyor11 <udoniyorbek99@mail.ru>
2021-06-15 11:14:21 +0500

    Api ulandi

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import FilterableProductTable from "./FilterableProductTable";
-const PRODUCTS = [
-    {
-        price: '$3.10', stocked: true, name: 'Shaftoli'
-    },
-    {
-        price: '$3.10', stocked: true, name: 'Gilos'
-    },
-    {
-        price: '$3.10', stocked: false, name: 'Nok'
-    },
-    {
-        price: '$3.10', stocked: true, name: 'Sut'
-    },
-    {
-        price: '$3.10', stocked: false, name: 'Olma'
-    },
-    {
-        price: '$3.10', stocked: true, name: 'Suv'
-    },
+import App from './app'
 
-]
+// const PRODUCTS = [
+//     {
+//         price: '$3.10', stocked: true, name: 'Shaftoli'
+//     },
+//     {
+//         price: '$3.10', stocked: true, name: 'Gilos'
+//     },
+//     {
+//         price: '$3.10', stocked: false, name: 'Nok'
+//     },
+//     {
+//         price: '$3.10', stocked: true, name: 'Sut'
+//     },
+//     {
+//         price: '$3.10', stocked: false, name: 'Olma'
+//     },
+//     {
+//         price: '$3.10', stocked: true, name: 'Suv'
+//     },
+//
+// ]
 
-ReactDOM.render( <FilterableProductTable products={PRODUCTS} />,
+// ReactDOM.render( <FilterableProductTable products={PRODUCTS} />,
+ReactDOM.render( <App />,
 document.getElementById('root'));
